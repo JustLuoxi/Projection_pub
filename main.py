df39893e01734aaa4da46feb050ad2d93301a7bb
JustLuoxi <luoxi@shanghaitech.edu.cn>
2022-05-12 02:15:16 +0800

    update main.py and readme

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from taichi.math import *
 scene = Scene(voxel_edges=0.01,  exposure=1.2)
 scene.set_floor(-64, (1.0,1.0,1.0))
 scene.set_background_color((0/255.0,0/255.0,0/255.0))
-scene.set_directional_light((0.0, 1.0,0.0), 0.0, (1, 1, 1))
+scene.set_directional_light((0.0, 1.0,0.0), 0.0, (1, 1, 1)) # ￥
+# scene.set_directional_light((-1.0, 0.0,0.0), 0.0, (1, 1, 1)) # €
+# scene.set_directional_light((0.0, 0.0,1.0), 0.0, (1, 1, 1)) # $
 @ti.func
 def colortable(id):
     color = vec3(1.0,1.0,1.0)
